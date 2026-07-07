2da2f52622118d71e53eee56a464d80d014ec74d
FrmJ <70322094+FrmJ@users.noreply.github.com>
2026-07-07 15:22:38 -0700

    add operate function and fix multiply function

--- script.js
+++ script.js
@@ -4,12 +4,28 @@ const add = (x, y) => {
 
 const subtract = (x, y) => {
     return x - y;
-}
+};
 
 const multiply = (x, y) => {
-    return x + y;
-}
+    return x * y;
+};
 
 const divide = (x, y) => {
     return x / y;
-}
+};
+
+const operate = (operator, x, y) => {
+    switch(operator) {
+        case "add":
+            return add(x, y);
+        
+        case "subtract":
+            return subtract(x, y);
+
+        case "multiply":
+            return multiply(x, y);
+
+        case "divide":
+            return divide(x, y);
+    }
+};
